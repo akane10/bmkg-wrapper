ecab5375ddad468b0dc7d7fcb48aea7bad980a51
yapie <targaryen.akane@gmail.com>
2020-08-03 21:10:22 +0700

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@ mod gempa;
 mod tests {
     use crate::gempa;
 
-    // #[test]
-    // fn get_data() {
-    //     // let result = ("Tanggal".to_string(), "30-Jul-20".to_string());
+    #[test]
+    fn get_data() {
+        // let result = ("Tanggal".to_string(), "30-Jul-20".to_string());
 
-    //     let data = gempa::get_data(gempa::Url::Autogempa).unwrap();
-    //     let (key, _) = &data[0][0];
+        let data = gempa::get_data(gempa::Url::Autogempa).unwrap();
+        let (key, _) = &data[0][0];
 
-    //     assert_eq!(key, &"Tanggal".to_string());
-    // }
+        assert_eq!(key, &"Tanggal".to_string());
+    }
 }
